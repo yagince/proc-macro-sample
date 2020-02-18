9622ee5034341098ffd6c91763a7dce10d6e000b
yagince <straitwalk@gmail.com>
2020-02-18 11:56:48 +0900

    [#master] add lowercase option

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,31 @@
 extern crate proc_macro;
-use quote::quote;
-use syn::{parse_macro_input, ItemStruct};
 
-#[proc_macro_derive(SelfName)]
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+use darling::{FromDeriveInput};
+
+#[derive(Debug, FromDeriveInput)]
+#[darling(attributes(self_name))]
+struct SelfNameOption {
+    #[darling(default)]
+    lowercase: bool,
+}
+
+#[proc_macro_derive(SelfName, attributes(self_name))]
 pub fn derive_self_name(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let item = parse_macro_input!(input as ItemStruct);
+    let item = parse_macro_input!(input as DeriveInput);
+    let option = SelfNameOption::from_derive_input(&item).unwrap();
     let struct_name = item.ident;
+    let mut name_str = struct_name.to_string();
+
+    if option.lowercase {
+        name_str = name_str.to_lowercase();
+    }
+
     let gen = quote! {
         impl #struct_name {
-            pub fn self_name(&self) -> String {
-                stringify!(#struct_name).to_owned()
+            pub fn self_name(&self) -> &str {
+                #name_str
             }
         }
     };
--- tests/test.rs
+++ tests/test.rs
@@ -8,3 +8,14 @@ fn test_hoge() {
     let hoge = Hoge {};
     assert_eq!(hoge.self_name(), "Hoge");
 }
+
+#[derive(Debug, Clone, PartialEq, SelfName)]
+#[self_name(lowercase)]
+struct HogeLowercase {}
+
+#[test]
+fn test_hoge_lowercase() {
+    let hoge = HogeLowercase {};
+    assert_eq!(hoge.self_name(), "hogelowercase");
+}
+
